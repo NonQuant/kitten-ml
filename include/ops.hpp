c25246e1635aa2e6132b139b5d5667a9e73c5d8f
NonQuant <husainov.21.04@gmail.com>
2026-09-25 23:46:43 +0500

    feat(ops): implement relu op

--- include/ops.hpp
+++ include/ops.hpp
@@ -15,4 +15,16 @@ inline TensorPtr transpose(const TensorPtr &a) {
 
   return out;
 }
+
+inline TensorPtr relu(const TensorPtr &a) {
+  auto out = std::make_shared<Tensor>(a->data.cwiseMax(0));
+
+  out->parents = {a};
+
+  out->backward_fn = [a, out]() {
+    a->grad += (out->grad.array() > 0).select(out->grad, 0);
+  };
+
+  return out;
+}
 } // namespace kittenml
